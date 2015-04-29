329ba25186b2a0e1b354dd22d82d4d13c1894837
Nelson Arturo Rodriguez Briceño <nelson.a.rodriguez@gmail.com>
2015-04-29 11:34:43 -0430

    Falto un modelo en el Commmit anterior

---
+++ Suma2Lealtad/Models/ExceptionJSON.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Suma2Lealtad.Models
+{
+    public class ExceptionJSON
+    {
+        public string code { get; set; }
+        public string detail { get; set; }
+        public string source { get; set; }
+    }
+
+}
