25cae2c13e1962b563574c61d95a2708be0a65d9
Nelson Arturo Rodriguez Briceño <nelson.a.rodriguez@gmail.com>
2015-06-30 11:48:42 -0430

    R4 Base para pruebas prepago y revisión de auditora de sistemas.

--- Suma2Lealtad/Controllers/HomeController.cs
+++ Suma2Lealtad/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Suma2Lealtad.Controllers
             AppSession app = new AppSession();
             if (app.Login(Model.UserName, Model.Password))
             {
-                if (app.UserLogin == "prepago")
+                if (app.UserLogin == "prepago" || app.UserLogin == "jmartinezprepago")
                 {
                     Session["titulo"] = "Administrador PREPAGO";
                 }
